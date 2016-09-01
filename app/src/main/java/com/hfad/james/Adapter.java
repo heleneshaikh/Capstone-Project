96d2ea30d6d31a59f759db9c2a9679f9ef56cac8
heleneshaikh <helene.shaikh@gmail.com>
2016-09-01 10:53:21 +0200

    add recyclerview

--- app/src/main/java/com/hfad/james/Adapter.java
+++ app/src/main/java/com/hfad/james/Adapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
  * Created by heleneshaikh on 30/08/16.
  */
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
     @Override
     public Adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
