1262500d2486308d1c0c2added9ca8064b434afb
heleneshaikh <helene.shaikh@gmail.com>
2016-10-07 17:13:05 +0200

    fix iab

--- app/src/main/java/com/hfad/james/MenuActivity.java
+++ app/src/main/java/com/hfad/james/MenuActivity.java
@@ -10,9 +10,16 @@ import android.support.v7.widget.Toolbar;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.hfad.james.util.IabHelper;
 import com.hfad.james.util.IabResult;
+import com.hfad.james.util.Inventory;
+import com.hfad.james.util.Purchase;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,6 +29,26 @@ public class MenuActivity extends AppCompatActivity {
     @BindView(R.id.include)
     Toolbar toolbar;
     IabHelper helper;
+    RelativeLayout container;
+    static final String ITEM_SKU = "android.test.purchased";
+    private static final String TAG = "Billing ";
+    private static final String CONSUMPTION = "consumption";
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        helper = new IabHelper(this, getString(R.string.my_api_key));
+        helper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            @Override
+            public void onIabSetupFinished(IabResult result) {
+                if (result.isSuccess()) {
+                    Log.v(TAG, "setup OK");
+                } else {
+                    Log.v(TAG, "setup NOT OK");
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +59,15 @@ public class MenuActivity extends AppCompatActivity {
             setSupportActionBar(toolbar);
             toolbar.setTitle(R.string.app_name);
         }
+        replaceFragment();
+        MobileAds.initialize(this.getApplicationContext(), getString(R.string.my_admob_key));
+    }
+
+    public void launchPurchase() {
+        helper.launchPurchaseFlow(this, ITEM_SKU, 10001, purchaseFinishedListener, "purchaseToken");
+    }
+
+    private void replaceFragment() {
         Fragment menuFragment = new MenuFragment();
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
@@ -47,4 +83,61 @@ public class MenuActivity extends AppCompatActivity {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
+    IabHelper.OnIabPurchaseFinishedListener purchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        @Override
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.v("onIabPurchase", result.toString());
+            if (result.isFailure()) {
+                errorToast();
+            } else if (purchase.getSku().equals(ITEM_SKU)) {
+                consumeItem();
+            }
+        }
+    };
+
+    private void consumeItem() {
+        helper.queryInventoryAsync(receivedInventoryListener);
+    }
+
+    private void errorToast() {
+        Toast toast = Toast.makeText(this, R.string.error_toast, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    IabHelper.QueryInventoryFinishedListener receivedInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+        @Override
+        public void onQueryInventoryFinished(IabResult result, Inventory inv) {
+            Log.v("QueryInventory", result.toString());
+            if (result.isSuccess()) {
+                helper.consumeAsync(inv.getPurchase(ITEM_SKU), consumeFinishedListener);
+            } else {
+                errorToast();
+            }
+        }
+    };
+
+    IabHelper.OnConsumeFinishedListener consumeFinishedListener = new IabHelper.OnConsumeFinishedListener() {
+        @Override
+        public void onConsumeFinished(Purchase purchase, IabResult result) {
+            if (result.isSuccess()) {
+                Log.v(CONSUMPTION, "consumption ok" + " purchase" + purchase + "result " + result);
+//                container.removeAllViews();
+                Toast.makeText(MenuActivity.this, "OnConsumeFinishedListener ok",Toast.LENGTH_LONG ).show();
+
+            } else {
+                errorToast();
+            }
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (helper != null) {
+            helper.dispose();
+            helper = null;
+        }
+    }
+
 }
--- app/src/main/java/com/hfad/james/MenuFragment.java
+++ app/src/main/java/com/hfad/james/MenuFragment.java
@@ -41,55 +41,21 @@ public class MenuFragment extends Fragment {
     RelativeLayout container;
     @BindView(R.id.adView)
     AdView adView;
-    //    static final String ITEM_SKU = "com.hfad.ads";
-    static final String ITEM_SKU = "android.test.purchased";
-    IabHelper helper;
-    private static final String TAG = "Billing ";
     private static final String STR = "String ";
-    private static final String CONSUMPTION = "consumption";
+
+    //    static final String ITEM_SKU = "com.hfad.ads";
 
     public MenuFragment() {
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        helper = new IabHelper(getActivity(), getString(R.string.my_api_key));
-//        helper.enableDebugLogging(true);
-        helper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
-            @Override
-            public void onIabSetupFinished(IabResult result) {
-                if (result.isSuccess()) {
-                    Log.v(TAG, "setup OK");
-                } else {
-                    Log.v(TAG, "setup NOT OK");
-                }
-            }
-        });
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        RelativeLayout relativeLayout = (RelativeLayout) inflater.inflate(R.layout.fragment_menu, container, false);
-        ButterKnife.bind(this, relativeLayout);
-        Log.v(STR, getString(R.string.my_api_key));
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        ButterKnife.bind(this, view);
 
-        String android_id = Settings.Secure.getString(getActivity().getContentResolver(),
-                Settings.Secure.ANDROID_ID);
-        Log.v("AndroidId", "Android ID : " + android_id);
-
-        MobileAds.initialize(getActivity().getApplicationContext(), getString(R.string.my_admob_key));
-
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);
-
-//        AdRequest adRequest = new AdRequest.Builder()
-//                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-//                .addTestDevice("b9c49844a26fd47b") //my id
-//                .addTestDevice("F88D920791452B0C2A6BA68A4A060E9F")
-//                .build();
-//        adView.loadAd(adRequest);
+        createRequest();
 
         foodButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -110,72 +76,27 @@ public class MenuFragment extends Fragment {
         removeAdButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                helper.launchPurchaseFlow(getActivity(), ITEM_SKU, 1001, purchaseFinishedListener, "purchaseToken");
+                ((MenuActivity) getActivity()).launchPurchase();
             }
         });
-        return relativeLayout;
+        return view;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.v("onActivityFragment", String.valueOf(resultCode));
-        if (!helper.handleActivityResult(requestCode, resultCode, data)) {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
+    private void createRequest() {
+        Log.v(STR, getString(R.string.my_api_key));
 
-    IabHelper.OnIabPurchaseFinishedListener purchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
-        @Override
-        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
-            Log.v("onIabPurchase", result.toString());
-            if (result.isFailure()) {
-                errorToast();
-            } else if (purchase.getSku().equals(ITEM_SKU)) {
-                consumeItem();
-            }
-        }
-    };
+        String android_id = Settings.Secure.getString(getActivity().getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+        Log.v("AndroidId", "Android ID : " + android_id);
 
-    private void consumeItem() {
-        helper.queryInventoryAsync(receivedInventoryListener);
-    }
-
-    private void errorToast() {
-        Toast toast = Toast.makeText(getActivity(), R.string.error_toast, Toast.LENGTH_LONG);
-        toast.show();
-    }
-
-    IabHelper.QueryInventoryFinishedListener receivedInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
-        @Override
-        public void onQueryInventoryFinished(IabResult result, Inventory inv) {
-            Log.v("QueryInventory", result.toString());
-            if (result.isSuccess()) {
-                helper.consumeAsync(inv.getPurchase(ITEM_SKU), consumeFinishedListener);
-            } else {
-                errorToast();
-            }
-        }
-    };
-
-    IabHelper.OnConsumeFinishedListener consumeFinishedListener = new IabHelper.OnConsumeFinishedListener() {
-        @Override
-        public void onConsumeFinished(Purchase purchase, IabResult result) {
-            if (result.isSuccess()) {
-                Log.v(CONSUMPTION, "consumption ok" + " purchase" + purchase + "result " + result);
-                container.removeAllViews();
-            } else {
-                errorToast();
-            }
-        }
-    };
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (helper != null) {
-            helper.dispose();
-            helper = null;
-        }
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+        //        AdRequest adRequest = new AdRequest.Builder()
+//                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+//                .addTestDevice("b9c49844a26fd47b") //my id
+//                .addTestDevice("F88D920791452B0C2A6BA68A4A060E9F")
+//                .build();
+//        adView.loadAd(adRequest);
     }
 
 
